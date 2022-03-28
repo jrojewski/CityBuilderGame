ccd09e51abb69366df4ced84aa6d6744d9e67bf0
jrojewski <57857936+jrojewski@users.noreply.github.com>
2022-03-28 19:40:30 +0200

    Initial commit

---
+++ Source/CityBuilderGame/CityBuilderGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CityBuilderGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CITYBUILDERGAME_API ACityBuilderGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
